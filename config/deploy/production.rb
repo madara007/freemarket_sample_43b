9c8caafcab07bc69bd2863ae944312377de8ed02
yuu0919 <yw3055.jenesice@gmail.com>
2019-01-28 14:41:36 +0900

    Retouch

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -62,6 +62,6 @@
 
 server '52.198.40.158', user: 'ec2-user', roles: %w{app db web}
 
-set :rails_env, "production"
-set :unicorn_rack_env, "production"
+# set :rails_env, "production"
+# set :unicorn_rack_env, "production"
 
