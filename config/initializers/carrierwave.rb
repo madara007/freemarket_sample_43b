a8c9fa2b44b1841e542a5f4bbbeb51f4ddb27a0e
yuu0919 <yw3055.jenesice@gmail.com>
2019-01-28 16:52:22 +0900

    Re:re:retouch

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,7 +4,7 @@ require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
   if Rails.env.development? || Rails.env.test?
-    config.strage = :file
+    config.storage = :file
   elsif Rails.env.production?
     config.storage = :fog
     config.fog_provider = 'fog/aws'
