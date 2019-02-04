2b4a5e8247e1ad6f7d04de01771b66557acf572a
山藤大 <masaru_0511_496@yahoo.co.jp>
2019-02-04 17:38:12 +0900

    編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  get "ads" => "users#edit"
   root 'items#index'
   get "users/registration" => "users#registration"
   get "users/select" => "users#select"
