12095747de61649c5373cff73e33eaea8d7447ab
Fippiy <profier4@gmail.com>
2019-02-05 18:21:55 +0900

    Delete select action and view-file

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,6 @@ class UsersController < ApplicationController
   def new
   end
 
-# ユーザーの新規登録選択画面への遷移(仮)
-  def select
-  end
-
 # ユーザーの新規登録画面(仮)
   def registration
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
   get "users/registration" => "users#registration"
-  get "users/select" => "users#select"
   resources :items
   resources :users
   resources :regions
