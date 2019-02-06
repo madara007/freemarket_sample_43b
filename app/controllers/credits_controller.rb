95c35b6de9e7260ceca4b02eca6f9ea4af7b5ba7
山藤大 <masaru_0511_496@yahoo.co.jp>
2019-02-06 12:11:37 +0900

    クレジットカード決済機能の追加

--- app/controllers/credits_controller.rb
+++ app/controllers/credits_controller.rb
@@ -1,8 +1,21 @@
 class CreditsController < ApplicationController
   layout  "session", only: [:index]
-  def new
-  end
 
-  def index
-  end
+   def new
+
+   end
+   
+   def index
+   
+   end
+
+
+   def create
+      Payjp.api_key = "sk_test_63fca0d2e14f02caa7fb939d"
+      charge = Payjp::Charge.create(
+      :amount => 3000,
+      :card => params['payjp-token'],
+      :currency => 'jpy',
+      )
+   end
 end
