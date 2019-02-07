ca9c6a4641b503c303d6f24f204a4ac687786511
山藤大 <masaru_0511_496@yahoo.co.jp>
2019-02-07 12:09:28 +0900

    編集しました

--- app/controllers/credits_controller.rb
+++ app/controllers/credits_controller.rb
@@ -1,6 +1,6 @@
 class CreditsController < ApplicationController
   layout  "session", only: [:index]
-
+  before_action :item_set, only: [:create]
    def new
 
    end
@@ -10,14 +10,18 @@ class CreditsController < ApplicationController
    end
 
    def create
-      item = Item.find(params[:id])
       price = item.price 
       Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
       charge = Payjp::Charge.create(
       :amount => price,
       :card => params['payjp-token'],
       :currency => 'jpy',
-
       )
+   end
+
+   private
+
+   def item_set
+     item = Item.find(params[:id])
    end
 end
