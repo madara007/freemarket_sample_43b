e52346ee56b244620c28828dd76d23257c205944
Fippiy <profier4@gmail.com>
2019-02-08 14:53:44 +0900

    rewrite delivery_fee enum column name

--- app/models/delivery_fee.rb
+++ app/models/delivery_fee.rb
@@ -1,5 +1,5 @@
 class DeliveryFee < ApplicationRecord
   has_many :items
   self.inheritance_column = :_type_disabled
-  enum type: [:select_condition, :postage_included, :freight_collect]
+  enum type: [:select_delivery_fee, :postage_included, :freight_collect]
 end
