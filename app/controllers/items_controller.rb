385c0ba0b8c9deb14c88ef7d5c98e6acf7fd604e
fukadashigeru <sakusaku721@gmail.com>
2019-02-08 17:38:44 +0900

    Items_controller.rbのアクションの順番修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -14,11 +14,12 @@ class ItemsController < ApplicationController
     @item.save
   end
 
+  def show
+  end
+
   def update
   end
 
-  def show
-  end
 
   def item_params
     params.require(:item).permit(:name, :price,:description, :category_id,:buyer_id, :saler_id, :shipping_date_id,:condition_id,:region_id, :delivery_fee_id, :ship_method_id, :brand_id,:size_id, :transaction)
