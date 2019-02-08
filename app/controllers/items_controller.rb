9cdcc5b9ebb7bcdd5346f530df6a1cd2c146ef97
yuu0919 <yw3055.jenesice@gmail.com>
2019-02-08 21:30:43 +0900

    Add-server-side

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,14 @@
 class ItemsController < ApplicationController
-  # トップページ作成でindex利用
+
   def index
+    @ladies = Item.where(category_id: 82).order("id DESC").first(4)
+    @menzes = Item.where(category_id: 83).order("id DESC").first(4)
+    @babies = Item.where(category_id: 84).order("id DESC").first(4)
+    @cosumes = Item.where(category_id: 85).order("id DESC").first(4)
+    @chanels = Item.where(brand_id: 1).order("id DESC").first(4)
+    @nikes = Item.where(brand_id: 2).order("id DESC").first(4)
+    @pumas = Item.where(brand_id: 3).order("id DESC").first(4)
+    @vuittons = Item.where(brand_id: 4).order("id DESC").first(4)
   end
 
   def new
