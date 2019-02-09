0f8f11e30e55ad5c34ebfe1d2dd58a6e07e2d0b7
fukadashigeru <45095615+fukadashigeru@users.noreply.github.com>
2019-02-09 20:51:50 +0900

    Update exhibit-complete.js
    console.logが残っていましたので、消去しました。

--- app/assets/javascripts/exhibit-complete.js
+++ app/assets/javascripts/exhibit-complete.js
@@ -4,16 +4,13 @@ $(function(){
         $modalOpen_2 = $(".btn-red-2"),
         $modalOverlay = $(".opacity_box"),
         $body = $("body");
-  console.log('aaa')
   $modalOpen.on({
     click: function () {
       this.blur();
       $body.addClass("is-fixed");
       $modalOverlay.addClass("is-visible");
       $modalContent.addClass("is-visible");
-      console.log('bbb')
     }
   });
   $modalOpen.prop('disabled', false);
-  console.log('ccc')
 });
