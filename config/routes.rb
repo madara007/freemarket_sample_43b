c6730fd4824d5ec4fe720f93439b1b03a0a8bb80
Fippiy <profier4@gmail.com>
2019-02-09 13:13:08 +0900

    Edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
   get "users/logout" => "users#logout"
-  resources :items
-  resources :users
-  resources :regions
-  resources :brands
-  resources :categorys
-  resources :credits
+  resources :items, except: [:edit, :destroy]
+  resources :users, only: [:index, :new, :edit, :show]
+  # resources :regions
+  # resources :brands
+  # resources :categorys
+  resources :credits, only: [:index, :new, :create]
 end
