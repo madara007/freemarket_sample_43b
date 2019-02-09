54a1da2ceb856253a32f51913ab8e7d8c7496501
山藤大 <masaru_0511_496@yahoo.co.jp>
2019-02-09 14:44:21 +0900

    編集

--- app/assets/javascripts/bxslider.js
+++ app/assets/javascripts/bxslider.js
@@ -6,4 +6,17 @@ $(function(){
       speed: 600
     });
   });
+  $(function(){
+    for (let i=1; i<=4; i++) {  //商品にカーソルを合わせると対象のイメージを表示する
+      let m = "#mini" + i
+      let b = "#big"  + i
+      $(m).mouseover(function() {
+        $('.mini').css("opacity", "0.5");
+        $(this).css('opacity', '1.0');
+        $(".bigs").not(b).css("display", "none");
+        $(b).fadeIn();
+      });
+    }
+  });
 });
+
--- app/controllers/credits_controller.rb
+++ app/controllers/credits_controller.rb
@@ -1,28 +1,25 @@
 class CreditsController < ApplicationController
   layout  "session", only: [:index]
   before_action :item_set, only: [:create]
-   def new
+  def index
+  end
 
-   end
-   
-   def index
-   
-   end
+  def new
+  end
 
-   def create
-      price = item.price 
-      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-      charge = Payjp::Charge.create(
+  def create
+  price = item.price
+  Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+  charge = Payjp::Charge.create(
+    amount: price,
+    card: params['payjp-token'],
+    currency: 'jpy',
+  )
+  end
 
-         amount: price,
-         card:  params['payjp-token'],
-         currency: 'jpy',
-      )
-   end
+  private
 
-   private
-
-   def item_set
-     item = Item.find(params[:id])
-   end
+  def item_set
+   item = Item.find(params[:id])
+  end
 end
