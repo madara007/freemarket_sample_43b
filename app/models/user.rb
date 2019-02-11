b94a05036a64d4541293763eec5c557553575d8c
Fippiy <profier4@gmail.com>
2019-02-11 19:46:06 +0900

    Setting users validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User < ApplicationRecord
   has_many :scores
   has_many :likes
   has_many :items
+  validates :nickname, presence: true
+  validates :encrypted_password, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :password, length: { in: 6..128 }
+  validates :birthday, presence: true
 end
