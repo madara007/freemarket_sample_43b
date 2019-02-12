d4f9420a62288ca2f3bf5452939a3202b5159e00
gonta1026 <katano.original@gmail.com>
2019-02-12 19:07:39 +0900

    マージ分（schema編集）

--- db/migrate/20190204043154_create_categories.rb
+++ db/migrate/20190204043154_create_categories.rb
@@ -3,6 +3,7 @@ class CreateCategories < ActiveRecord::Migration[5.2]
     create_table :categories do |t|
       t.integer :name, null: false
       t.integer :parent_id
+      t.integer :group
       t.timestamps
     end
   end
