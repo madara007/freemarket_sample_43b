c8b43b59ba21ea2c9c2ddd555635d801416229bd
gonta1026 <katano.original@gmail.com>
2019-02-14 19:34:34 +0900

    画像を保存

--- app/assets/javascripts/image_upload.js
+++ app/assets/javascripts/image_upload.js
@@ -9,7 +9,6 @@ $(function() {
       }
       reader.onload = (function(file) {
         return function(e) {
-          $preview.empty();
           $preview.append($('<img>').attr({
                     src: e.target.result,
                     width: "143px",
