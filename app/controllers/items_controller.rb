60ac41def0795b3ae9890e1ae61caa3e6d8a5d24
fukadashigeru <sakusaku721@gmail.com>
2019-02-15 12:59:43 +0900

    微修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -48,7 +48,6 @@ class ItemsController < ApplicationController
   end
 
   def item_params
-    brand_id =
     params.require(:item).permit(:name, :price, :description, :category_id, :shipping_date_id, :condition_id, :region_id, :delivery_fee_id, :ship_method_id, :brand_id, :size_id, :transaction, item_photos_attributes: [:id, :photo]).merge(saler_id: current_user.id, brand_id: (Brand.find_by(name: params[:item][:brand_name]).present?) ? (Brand.find_by(name: params[:item][:brand_name]).id) : nil)
   end
 
