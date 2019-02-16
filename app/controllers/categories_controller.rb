2bcacb544a0a633d28503db0440555e6c380bf3e
yuu0919 <yw3055.jenesice@gmail.com>
2019-02-16 16:03:04 +0900

    create category show-page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,8 +4,10 @@ class CategoriesController < ApplicationController
     @categories = Category.roots
   end
 
-  def  show
+  def show
     @category = Category.find(params[:id])
+    @children = get_categories_children
+    @descendants = get_categories_descendants
   end
 
   def search
@@ -13,5 +15,19 @@ class CategoriesController < ApplicationController
     respond_to do |format|
       format.json
     end
+  end
+
+  private
+
+  def get_items_category(id)
+    return Item.where(category_id: id).order("id DESC")
+  end
+
+  def get_categories_children
+    return Category.find(params[:id]).self_and_children
+  end
+
+  def get_categories_descendants
+    return Category.find(params[:id]).self_and_descendants
   end
 end
