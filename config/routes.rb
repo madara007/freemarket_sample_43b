50bb4cd62beb5ae00af70d3ef5ae7850e1ce8024
gonta1026 <katano.original@gmail.com>
2019-02-19 12:04:24 +0900

    ルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   root 'items#index'
   devise_for :users, controllers: { sessions: 'sessions' ,registrations: "registrations"}
-  resources :items, except: [:edit, :destroy]
+  resources :items, except: [:edit, :destroy] do
+    collection do
+      get 'search'
+    end
+  end
   resources :users do
     collection do
       get "selling"
