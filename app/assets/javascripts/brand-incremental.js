428905bfe3b309bc82c053e059b73a4aeb88b7aa
gonta1026 <katano.original@gmail.com>
2019-02-19 12:07:41 +0900

    ajaxの実装

---
+++ app/assets/javascripts/brand-incremental.js
@@ -0,0 +1,38 @@
+$(function() {
+  function appendBrand(brand) {
+    var search_list = $(".brand-box");
+    var html = `<div class="brands">
+                  <p class="brand-name data-brand-id="${brand.id} data-brand-name="${brand.name}">${brand.name}</p>
+                </div>`
+    search_list.append(html);
+  }
+
+  $(document).on("keyup", ".input-brand", function() {
+    var input = $(this).val();
+        console.log(this);
+       $(".brand-box").empty();
+    if (input !== ""){
+     $.ajax({
+        type: "GET",
+        url: "/items/search",
+        dataType: "json",
+        data: { keyword: input },
+      })
+     .done(function(brands) {
+       $(".brand-box").empty();
+       if (brands.length !== 0) {
+         brands.forEach(function(brand){
+           appendBrand(brand);
+         });
+       }
+     })
+    }
+  });
+
+  $(document).on("click",".brand-name", function(){
+    var brand_id = $(this).attr('data-brand-id');
+    var name = $(this).attr('data-brand-name');
+    $(".brands").css("display", "none");
+    $(".input-brand").val(name, brand_id);
+  });
+});
