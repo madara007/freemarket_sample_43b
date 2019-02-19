7230c1f4e1d7458f142ebf9f27ee0734ce8c8d20
Fippiy <profier4@gmail.com>
2019-02-19 17:20:25 +0900

    Create scores route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'sessions' ,registrations: "registrations"}
   resources :items, except: [:edit, :destroy] do
     resources :comments, only: [:create]
+    resources :scores, only:[:new, :create]
   end
   resources :users do
     collection do
