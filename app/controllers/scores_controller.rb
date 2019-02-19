bcb90ab7d1e4c9aefa91847be05fbef19015f7d7
Fippiy <profier4@gmail.com>
2019-02-19 18:44:16 +0900

    Validate sennting to controller score

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -2,13 +2,18 @@ class ScoresController < ApplicationController
 
   def new
     @item = Item.find(params[:item_id])
+    @score = Score.new
   end
 
   def create
-    item = Item.find(score_params[:item_id])
-    Score.create(type: score_params[:type], user_id: item.saler_id)
-    item.update(trading: 3)
-    redirect_to :root
+    @item = Item.find(score_params[:item_id])
+    @score = Score.new(type: score_params[:type], user_id: @item.saler_id)
+    if @score.save
+      @item.update(trading: 3)
+      redirect_to :root
+    else
+      render "new"
+    end
   end
 
   private
