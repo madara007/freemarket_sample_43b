0142d3d857670606e5feb2ef4afedde2d62fa11e
gonta1026 <katano.original@gmail.com>
2019-02-20 19:32:55 +0900

    Itemモデルにdestroyアクションを対応

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,10 +7,10 @@ class Item < ApplicationRecord
   belongs_to :delivery_fee
   belongs_to :category
   belongs_to :brand ,optional: true
-  has_many :comments
-  has_many :item_photos
+  has_many :comments, dependent: :destroy
+  has_many :item_photos, dependent: :destroy
   accepts_nested_attributes_for :item_photos
-  has_many :likes
+  has_many :likes, dependent: :destroy
 
   belongs_to :saler, class_name: "User" ,optional: true
   belongs_to :buyer, class_name: "User" ,optional: true
