e54eec9751708bd87e0012c4eb2654b1e85bf4bc
Fippiy <profier4@gmail.com>
2019-02-25 17:24:38 +0900

    Add validate to profile model

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,2 +1,10 @@
 class Profile < ApplicationRecord
+  belongs_to :user
+  VALID_KANA_REGEX = /[ァ-ヴ][ァ-ヴー・]*/
+  validates :nickname, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true, format: { with: VALID_KANA_REGEX , message: "カナで入力してください。" }
+  validates :first_name_kana, presence: true, format: { with: VALID_KANA_REGEX , message: "カナで入力してください。" }
+  validates :birthday, presence: true
 end
