516b8b6c560ddbdd5e373975d713ff8840dee9e9
Fippiy <profier4@gmail.com>
2019-02-26 18:48:38 +0900

    rewrite routes to omniauth setting

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
   root 'items#index'
-  devise_for :users, controllers: { sessions: 'sessions' ,registrations: "registrations"}
-  resources :items, except: [:edit] do
+  # devise_for :users, controllers: { sessions: 'sessions' ,registrations: "registrations"}
+  devise_for :users, controllers: {
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    sessions: 'users/sessions',
+    registrations: "users/registrations"
+  }
+  devise_scope :user do
+    delete :sign_out, to: 'devise/sessions#destroy'
+    # , as: :destroy_user_session
+  end
+
+    resources :items, except: [:edit] do
     resources :likes, only: [:create, :destroy]
     resources :comments, only: [:create]
     resources :scores, only:[:new, :create]
