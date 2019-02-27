151270ad884510f379dbf1afaa778b50fea7610e
Fippiy <profier4@gmail.com>
2019-02-27 14:28:43 +0900

    Delete commentout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root 'items#index'
-  # devise_for :users, controllers: { sessions: 'sessions' ,registrations: "registrations"}
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks',
     sessions: 'users/sessions',
