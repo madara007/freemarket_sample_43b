44f08468b4e19ffd5de92bbc9f3a601d976f7ba4
Fippiy <profier4@gmail.com>
2019-02-27 11:48:26 +0900

    rewrite validate to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,16 +9,16 @@ class User < ApplicationRecord
   has_many :likes
   has_many :items, foreign_key: 'saler_id'
   has_many :comments
-  # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   # VALID_KANA_REGEX = /[ァ-ヴ][ァ-ヴー・]*/
-  # validates :nickname, presence: true
-  # validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
-  # validates :encrypted_password, presence: true
+  validates :nickname, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+  validates :encrypted_password, presence: true
   # validates :last_name, presence: true
   # validates :first_name, presence: true
   # validates :last_name_kana, presence: true, format: { with: VALID_KANA_REGEX , message: "カナで入力してください。" }
   # validates :first_name_kana, presence: true, format: { with: VALID_KANA_REGEX , message: "カナで入力してください。" }
-  # validates :password, length: { in: 6..128, message: "パスワードは6文字以上128文字以下で入力してください" }
+  validates :password, length: { in: 6..128, message: "パスワードは6文字以上128文字以下で入力してください" }
   # validates :birthday, presence: true
 
   def self.find_for_oauth(auth)
