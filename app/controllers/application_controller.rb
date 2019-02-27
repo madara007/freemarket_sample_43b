2ca59a4d5d8b93a233cf79c80bfe2fb572baef70
Fippiy <profier4@gmail.com>
2019-02-27 13:23:06 +0900

    Delete application controller settings

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -52,9 +52,5 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, profile_attributes: [:last_name, :first_name, :last_name_kana, :first_name_kana, :birthday]])
-  end
-
 end
 
