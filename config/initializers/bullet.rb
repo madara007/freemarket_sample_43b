9f9b579a8d0d234525c1b122dbe5b545fffafed1
Fippiy <profier4@gmail.com>
2019-03-03 13:57:44 +0900

    Delete snsliks setting to bullet

--- config/initializers/bullet.rb
+++ config/initializers/bullet.rb
@@ -1,6 +1,4 @@
 if Rails.env.development? || Rails.env.test?
   Bullet.add_whitelist type: :n_plus_one_query, class_name: "Category", association: :children
   Bullet.add_whitelist type: :unused_eager_loading, class_name: "Category", association: :children
-  Bullet.add_whitelist type: :n_plus_one_query, class_name: 'Item', association: :snslikes
-  Bullet.add_whitelist type: :unused_eager_loading, class_name: 'Item', association: :snslikes
 end
