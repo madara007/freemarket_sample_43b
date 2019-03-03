6088fbab4bae7a88b255e67c97a8872847f41290
Fippiy <profier4@gmail.com>
2019-03-03 12:38:40 +0900

    Delete liks controller settings

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,38 +1,19 @@
 class LikesController < ApplicationController
-  before_action :set_item , only: [:create, :destroy]
-  layout false, only: [:create, :destroy]
+  before_action :set_item
+  layout false
 
   def create
-    if current_user
-      @like = current_user.likes.create(item_id: params[:item_id])
-      respond_to do |format|
-        format.html
-        format.js
-      end
-    else
-      @like = sns_user.snslikes.create(item_id: params[:item_id])
-      respond_to do |format|
-        format.html
-        format.js
-      end
+    like = current_user.likes.create(item_id: params[:item_id])
+    respond_to do |format|
+      format.js
     end
   end
 
   def destroy
-    if current_user
-      like = current_user.likes.find_by(item_id: params[:item_id])
-      like.destroy
-      respond_to do |format|
-        format.html
-        format.js
-      end
-    else
-      like = sns_user.snslikes.find_by(item_id: params[:item_id])
-      like.destroy
-      respond_to do |format|
-        format.html
-        format.js
-      end
+    like = current_user.likes.find_by(item_id: params[:item_id])
+    like.destroy
+    respond_to do |format|
+      format.js
     end
   end
 
